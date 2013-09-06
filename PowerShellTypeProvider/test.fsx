b001523470c9867a283ab9c7501c02d417772b21
Sergey Tihon <sergey.tihon@gmail.com>
2013-09-06 20:59:34 +0300

    Test script for TP debug was added

---
+++ PowerShellTypeProvider/test.fsx
@@ -0,0 +1,6 @@
+#r @"C:\Windows\Microsoft.NET\assembly\GAC_MSIL\System.Management.Automation\v4.0_3.0.0.0__31bf3856ad364e35\System.Management.Automation.dll"
+#r @"bin\Debug\PowerShellTypeProvider.dll"
+
+type PS = FSharp.PowerShell.PowerShellTypeProvider< PSSnapIns="", Is64BitRequired=false >
+
+PS.``Get-Item``()
